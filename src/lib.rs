bea777613ddfd468f855980ff93c834b9295442f
Beelzebub <enerrr.wsed@gmail.com>
2025-11-10 07:41:15 +0300

    Refactor mask_fs_scan to use unsafe block

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,14 @@ use std::ffi::CString;
 fn gmod_open(lua: LuaState) -> Result<i32> {
 	data::scan(lua)?;
 
-	unsafe extern "C" fn mask_fs_scan(lua: LuaState) -> i32 {
+	extern "C" fn mask_fs_scan(lua: LuaState) -> i32 {
 		if let Err(err) = data::scan(lua) {
-			let msg = CString::new(format!("mask_fs_scan failed: {}", err))
-				.unwrap_or_else(|_| CString::new("mask_fs_scan failed").unwrap());
-			lua_pushstring(lua, msg.as_ptr());
-			lua_error(lua);
+			unsafe {
+				let msg = CString::new(format!("mask_fs_scan failed: {}", err))
+					.unwrap_or_else(|_| CString::new("mask_fs_scan failed").unwrap());
+				lua_pushstring(lua, msg.as_ptr());
+				lua_error(lua);
+			}
 		}
 		0
 	}
