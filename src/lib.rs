c226c7e81fbe6bc56a5c4ebfe9492c91cb755948
Beelzebub <enerrr.wsed@gmail.com>
2025-11-10 07:31:30 +0300

    idk what i do XD

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,23 @@ mod resource;
 
 use anyhow::Result;
 use rglua::prelude::*;
+use std::ffi::CString;
 
 #[gmod_open]
 fn gmod_open(lua: LuaState) -> Result<i32> {
 	data::scan(lua)?;
 
-	lua_pushcfunction(lua, Some(|lua| -> i32 {
+	unsafe extern "C" fn mask_fs_scan(lua: LuaState) -> i32 {
 		if let Err(err) = data::scan(lua) {
-			lua_pushstring(lua, format!("mask_fs_scan failed: {}", err));
+			let msg = CString::new(format!("mask_fs_scan failed: {}", err))
+				.unwrap_or_else(|_| CString::new("mask_fs_scan failed").unwrap());
+			lua_pushstring(lua, msg.as_ptr());
 			lua_error(lua);
 		}
 		0
-	}));
+	}
+
+	lua_pushcfunction(lua, Some(mask_fs_scan));
 	lua_setglobal(lua, cstr!("mask_fs_scan"));
 
 	Ok(0)
