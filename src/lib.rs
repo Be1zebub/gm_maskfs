bb512190a92540d6bdcdac04416489d9137a3779
Beelzebub <enerrr.wsed@gmail.com>
2025-11-10 07:26:45 +0300

    Add mask_fs_scan global to re-scan without server restart

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,21 @@ use rglua::prelude::*;
 
 #[gmod_open]
 fn gmod_open(lua: LuaState) -> Result<i32> {
-  data::scan(lua)?;
+	data::scan(lua)?;
 
-  Ok(0)
+	lua_pushcfunction(lua, Some(|lua| -> i32 {
+		if let Err(err) = data::scan(lua) {
+			lua_pushstring(lua, format!("mask_fs_scan failed: {}", err));
+			lua_error(lua);
+		}
+		0
+	}));
+	lua_setglobal(lua, cstr!("mask_fs_scan"));
+
+	Ok(0)
 }
 
 #[gmod_close]
 fn gmod_close(_lua: LuaState) -> i32 {
-  0
+	0
 }
